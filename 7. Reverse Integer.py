07a50746f97cc060439b984700eb231b9ad980e7
Tian Feng <feng.txy@outlook.com>
2019-08-05 10:12:20 -0700

    Create 7. Reverse Integer.py

---
+++ 7. Reverse Integer.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        sign = 1 if x>0 else -1
+        x = abs(x)
+        res = 0
+        while x>9:
+            res += x%10
+            res*=10
+            x //= 10
+        res = (res+x)*sign
+        return res if -(2**31)<= res<= (2**31)-1 else 0
+            
+        
