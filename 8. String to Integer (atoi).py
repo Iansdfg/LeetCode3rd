9c888f408f787c7b7312009302034303ec601dfe
Tian Feng <feng.txy@outlook.com>
2019-08-07 23:42:48 -0700

    Update 8. String to Integer (atoi).py

--- 8. String to Integer (atoi).py
+++ 8. String to Integer (atoi).py
@@ -1,22 +1,20 @@
 class Solution(object):
-    def myAtoi(self, str):
+    def myAtoi(self, strr):
         """
         :type str: str
         :rtype: int
         """
-        str=str.split()
-        if len(str)==0: return 0
-        s=str[0]
-        i=0
-        sign=1
-        if s[0]=='-' or s[0]=='+':
-            sign=-1 if s[0]=='-' else 1
-            i+=1  
-        res=0
-        while i<len(s):
-            if ord(s[i])<ord('0') or ord(s[i])>ord('9'):
-                break
+        strr = strr.split()
+        if len(strr) == 0:return 0
+        s = strr[0]
+        sign = 1
+        if s[0] == '-' or s[0] == '+':
+            sign = -1 if s[0] == '-'else 1
+            s = s[1:]
+        res = 0 
+        for i in range(len(s)):
+            if s[i].isdigit():
+                res = res*10 + int(s[i])
             else:
-                res=res*10+ord(s[i])-ord('0')
-            i+=1
+                return max(min(res* sign, 2147483647),-2147483648) 
         return max(min(res* sign, 2147483647),-2147483648) 
