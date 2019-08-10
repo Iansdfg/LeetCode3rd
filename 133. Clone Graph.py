072dc329cb3f9c3fdc808007e730ff79b81fd5e5
Tian Feng <feng.txy@outlook.com>
2019-08-10 12:53:41 -0700

    Create 133. Clone Graph.py

---
+++ 133. Clone Graph.py
@@ -0,0 +1,33 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, neighbors):
+        self.val = val
+        self.neighbors = neighbors
+"""
+class Solution(object):
+    def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
+        visited = [node]
+        queue = collections.deque()
+        queue.append(node)
+        node_dic = dict()
+        node_dic[node] = Node(node.val,[])
+        while queue:
+            curr_node = queue.popleft()
+            for neighbor in curr_node.neighbors :
+                if neighbor not in visited:
+                    copy_node = Node(neighbor.val,[])
+                    node_dic[neighbor] = copy_node
+                    visited.append(neighbor)
+                    queue.append(neighbor)
+        for dic_key_node in node_dic:
+            for neighbor in dic_key_node.neighbors:
+                node_dic[dic_key_node].neighbors.append(node_dic[neighbor])
+                
+        return node_dic[node]
+        
+        
