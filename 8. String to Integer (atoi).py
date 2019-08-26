7e3a30777d34c2bbec28a04114bef13c81e4d372
Tian Feng <feng.txy@outlook.com>
2019-08-26 12:10:53 +0900

    Update 8. String to Integer (atoi).py

--- 8. String to Integer (atoi).py
+++ 8. String to Integer (atoi).py
@@ -8,15 +8,19 @@ class Solution(object):
         if len(strr) == 0:return 0
         s = strr[0]
         sign = 1
+        
         if s[0] == '-' or s[0] == '+':
             sign = -1 if s[0] == '-'else 1
             s = s[1:]
         res = 0 
+        
         for i in range(len(s)):
             if s[i].isdigit():
                 res = res*10 + ord(s[i])-ord('0')
             else:
                 break 
+                
+                
         return max(min(res* sign, 2147483647),-2147483648) 
             
             
